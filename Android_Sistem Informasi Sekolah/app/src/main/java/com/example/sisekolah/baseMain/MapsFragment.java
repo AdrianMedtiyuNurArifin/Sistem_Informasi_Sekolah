0507f22d5f77b378226f442a1d5b617a560f8376
Onky Wijaya <rainine26@gmail.com>
2022-06-12 22:59:25 +0700

    Aplikasi Android

---
+++ Android_Sistem Informasi Sekolah/app/src/main/java/com/example/sisekolah/baseMain/MapsFragment.java
@@ -0,0 +1,69 @@
+package com.example.sisekolah.baseMain;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.core.app.ActivityCompat;
+import androidx.fragment.app.Fragment;
+
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.sisekolah.R;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class MapsFragment extends Fragment {
+    final private int REQUEST_COURSE_ACCESS = 123;
+    boolean permissionGranted = false;
+    private GoogleMap mMap;
+    LocationManager lm;
+    LocationListener locationListener;
+
+    private OnMapReadyCallback callback = new OnMapReadyCallback() {
+
+        /**
+         * Manipulates the map once available.
+         * This callback is triggered when the map is ready to be used.
+         * This is where we can add markers or lines, add listeners or move the camera.
+         * In this case, we just add a marker near Sydney, Australia.
+         * If Google Play services is not installed on the device, the user will be prompted to
+         * install it inside the SupportMapFragment. This method will only be triggered once the
+         * user has installed Google Play services and returned to the app.
+         */
+        //Menandai lokasi SMPN 1 Candipuro pada map
+        @Override
+        public void onMapReady(GoogleMap googleMap) {
+            LatLng lokasi = new LatLng(-8.05788583134154, 113.29831123352051);
+            googleMap.addMarker(new MarkerOptions().position(lokasi).title("MTs. Muhammadiyah 01 Randuagung"));
+            googleMap.moveCamera(CameraUpdateFactory.newLatLng(lokasi));
+        }
+    };
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_maps, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        SupportMapFragment mapFragment =
+                (SupportMapFragment) getChildFragmentManager().findFragmentById(R.id.map);
+        if (mapFragment != null) {
+            mapFragment.getMapAsync(callback);
+        }
+    }
+}
