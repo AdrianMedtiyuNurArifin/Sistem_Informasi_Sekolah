343106e5c80f4dd453d7758c43e1eaf2fa3f2998
Onky Wijaya <rainine26@gmail.com>
2022-06-12 23:11:13 +0700

    Revert "Aplikasi Android"
    This reverts commit 0507f22d5f77b378226f442a1d5b617a560f8376.

--- Android_Sistem Informasi Sekolah/app/src/main/java/com/example/sisekolah/PengumumanListener.java
+++
@@ -1,7 +0,0 @@
-package com.example.sisekolah;
-
-public interface PengumumanListener {
-//    void respond(String id, String judul, String isi);
-
-    void respond(int Postion, String id, String judul, String isi);
-}
